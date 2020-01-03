5480afcf5572a7a0e1b519a69ac2b07f7a4701ff
minhpham <demonhuter90@gmail.com>
2020-01-03 15:11:07 +0700

    Jpeg now

--- src/minh/yuhnim/ImageProcesser.java
+++ src/minh/yuhnim/ImageProcesser.java
@@ -8,6 +8,27 @@ public class ImageProcesser {
     public int imageHeight;
     public int imageWidth;
 
+    private int[][] lumi_quantizier = {
+            {16, 11, 10, 16, 24, 40, 51, 61},
+            {12, 12, 14, 19, 26, 58, 60, 55},
+            {14, 13, 16, 24, 40, 57, 69, 56},
+            {14, 17, 22, 29, 51, 87, 80, 62},
+            {18, 22, 37, 56, 68, 109, 103, 77},
+            {24, 35, 55, 64, 81, 104, 113, 92},
+            {49, 64, 78, 87, 103, 121, 120, 101},
+            {72, 92, 95, 98, 112, 100, 103, 99}};
+
+    private int[][] chroma_quantizier = {
+            {17, 18, 24, 47, 99, 99, 99, 99},
+            {18, 21, 26, 66, 99, 99, 99, 99},
+            {24, 26, 56, 99, 99, 99, 99, 99},
+            {47, 66, 99, 99, 99, 99, 99, 99},
+            {99, 99, 99, 99, 99, 99, 99, 99},
+            {99, 99, 99, 99, 99, 99, 99, 99},
+            {99, 99, 99, 99, 99, 99, 99, 99},
+            {99, 99, 99, 99, 99, 99, 99, 99}
+    };
+
     public ImageProcesser() {
 
     }
@@ -19,9 +40,9 @@ public class ImageProcesser {
         System.out.println("Loaded!");
     }
 
-    public int[][] getRGBArray(BufferedImage image) {
+    public int[][] getRGBArray() {
         int[][] rgbArray = new int[imageHeight][imageWidth];
-         for (int i = 0; i < imageHeight; i++) {
+        for (int i = 0; i < imageHeight; i++) {
             for (int j = 0; j < imageWidth; j++) {
                 rgbArray[j][i] = image.getRGB(i, j);
             }
@@ -31,7 +52,7 @@ public class ImageProcesser {
 
     public int[][] GetRedArray(int[][] rgbArray) {
         int[][] redArray = new int[imageHeight][imageHeight];
-         for (int i = 0; i < imageHeight; i++) {
+        for (int i = 0; i < imageHeight; i++) {
             for (int j = 0; j < imageWidth; j++) {
                 redArray[i][j] = (rgbArray[i][j] >> 16) & 0xff;
             }
@@ -42,7 +63,7 @@ public class ImageProcesser {
 
     public int[][] GetGreenArray(int[][] rgbArray) {
         int[][] greenArray = new int[imageHeight][imageHeight];
-         for (int i = 0; i < imageHeight; i++) {
+        for (int i = 0; i < imageHeight; i++) {
             for (int j = 0; j < imageWidth; j++) {
                 greenArray[i][j] = (rgbArray[i][j] >> 8) & 0xff;
             }
@@ -53,7 +74,7 @@ public class ImageProcesser {
 
     public int[][] GetBlueArray(int[][] rgbArray) {
         int[][] blueArray = new int[imageHeight][imageHeight];
-         for (int i = 0; i < imageHeight; i++) {
+        for (int i = 0; i < imageHeight; i++) {
             for (int j = 0; j < imageWidth; j++) {
                 blueArray[i][j] = (rgbArray[i][j]) & 0xff;
             }
@@ -69,7 +90,7 @@ public class ImageProcesser {
 
     public int[][] ConvertRGBToY(int[][] redArray, int[][] greenArray, int[][] blueArray) {
         int[][] Y = new int[imageHeight][imageWidth];
-         for (int i = 0; i < imageHeight; i++) {
+        for (int i = 0; i < imageHeight; i++) {
             for (int j = 0; j < imageWidth; j++) {
                 double y = kr * redArray[i][j] + kg * greenArray[i][j] + kb * blueArray[i][j];
                 Y[i][j] = (int) (y);
@@ -81,7 +102,7 @@ public class ImageProcesser {
 
     public int[][] ConvertRGBToCb(int[][] redArray, int[][] greenArray, int[][] blueArray) {
         int[][] Cb = new int[imageHeight][imageHeight];
-         for (int i = 0; i < imageHeight; i++) {
+        for (int i = 0; i < imageHeight; i++) {
             for (int j = 0; j < imageWidth; j++) {
                 double y = kr * redArray[i][j] + kg * greenArray[i][j] + kb * blueArray[i][j];
                 Cb[i][j] = (int) ((blueArray[i][j] - y) / (2 * (1 - kb)));
@@ -93,7 +114,7 @@ public class ImageProcesser {
 
     public int[][] ConvertRGBToCr(int[][] redArray, int[][] greenArray, int[][] blueArray) {
         int[][] Cr = new int[imageHeight][imageHeight];
-         for (int i = 0; i < imageHeight; i++) {
+        for (int i = 0; i < imageHeight; i++) {
             for (int j = 0; j < imageWidth; j++) {
                 double y = kr * redArray[i][j] + kg * greenArray[i][j] + kb * blueArray[i][j];
                 Cr[i][j] = (int) ((redArray[i][j] - y) / (2 * (1 - kr)));
@@ -107,34 +128,112 @@ public class ImageProcesser {
         int[][] redArray = new int[imageHeight][imageWidth];
         for (int i = 0; i < imageHeight; i++) {
             for (int j = 0; j < imageWidth; j++) {
-                redArray[i][j] = (int) (Y[i][j] + 1.402*(Cr[i][j] -128));
+                redArray[i][j] = (int) (Y[i][j] + 1.402 * (Cr[i][j] - 128));
             }
         }
         return redArray;
     }
+
     public int[][] ConvertYCbCrToG(int[][] Y, int[][] Cb, int[][] Cr) {
         int[][] greenArray = new int[imageHeight][imageWidth];
         for (int i = 0; i < imageHeight; i++) {
             for (int j = 0; j < imageWidth; j++) {
-                greenArray[i][j] = (int) (Y[i][j] - 0.34414*(Cb[i][j] -128)- 0.71414*(Cr[i][j] -128));
+                greenArray[i][j] = (int) (Y[i][j] - 0.34414 * (Cb[i][j] - 128) - 0.71414 * (Cr[i][j] - 128));
             }
         }
         return greenArray;
     }
+
     public int[][] ConvertYCbCrToB(int[][] Y, int[][] Cb, int[][] Cr) {
         int[][] blueArray = new int[imageHeight][imageWidth];
         for (int i = 0; i < imageHeight; i++) {
             for (int j = 0; j < imageWidth; j++) {
-                blueArray[i][j] = (int) (Y[i][j] + 1.772*(Cb[i][j] -128));
+                blueArray[i][j] = (int) (Y[i][j] + 1.772 * (Cb[i][j] - 128));
             }
         }
         return blueArray;
     }
 
-    public static void PrintMatrix(int[][] matrix) {
+    public void PrintMatrix(int[][] matrix) {
+        for (int i = 0; i < imageHeight; i++) {
+            for (int j = 0; j < imageWidth; j++) {
+                System.out.print(" | " + matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void showRGB(int[][] grbMatrix, int x, int y) {
+        System.out.println();
+        int red = (grbMatrix[x][y] & 0xff0000) >> 16;
+        System.out.println(red);
+        int green = (grbMatrix[x][y] & 0xff00) >> 8;
+        System.out.println(green);
+        int blue = (grbMatrix[x][y] & 0xff);
+        System.out.println(blue);
+    }
+
+    public static void showRGB(int[][] grbMatrix, int row, int column, int f) {
+        for (int i = 0; i < column; i++) {
+            for (int j = 0; j < row; j++) {
+                int red = (grbMatrix[i][j] & 0xff0000) >> 16;
+                System.out.print(red + " ");
+                int green = (grbMatrix[i][j] & 0xff00) >> 8;
+                System.out.print(green + " ");
+                int blue = (grbMatrix[i][j] & 0xff);
+                System.out.print(blue + " \t");
+            }
+            System.out.println();
+        }
+    }
+
+    public int[][] shift(int[][] a) {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                System.out.print(" | " + matrix[i][j]);
+                a[i][j] -= 128;
+            }
+        }
+        return a;
+    }
+
+    public double[][] DCT(int[][] g) {
+
+        double[][] G = new double[8][8];
+        double Cu;
+        double Cv;
+        for (int u = 0; u < 8; u++) {
+            for (int v = 0; v < 8; v++) {
+                int temp = 0;
+                for (int x = 0; x < 8; x++) {
+                    for (int y = 0; y < 8; y++) {
+                        temp += g[x][y] * Math.cos(((2.0 * x + 1.0) * u * Math.PI) / 16.0) * Math.cos(((2.0 * y + 1.0) * v * Math.PI) / 16.0);
+                    }
+                }
+                if (u == 0) Cu = 1 / Math.sqrt(2);
+                else Cu = 1;
+                if (v == 0) Cv = 1 / Math.sqrt(2);
+                else Cv = 1;
+                G[u][v] = Cu * Cv * temp / 4;
+            }
+        }
+        return G;
+    }
+
+    public int[][] quant(int[][] g) {
+        double[][] G = DCT(g);
+        int[][] Q = new int[8][8];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                Q[i][j] = (int) Math.round(G[i][j]/lumi_quantizier[i][j]);
+            }
+        }
+        return Q;
+    }
+
+    private void PrintMatrix(double[][] g) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print(g[i][j] + " | ");
             }
             System.out.println();
         }
--- src/minh/yuhnim/Main.java
+++ src/minh/yuhnim/Main.java
@@ -10,82 +10,34 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-	// write your code here
         ImageProcesser imageProcesser = new ImageProcesser();
-        System.out.println("Hello world!");
-        BufferedImage img = null;
-//        int[] rgbArray = new int[1000];
-        try {
-            File imgFile = new File("res/pic1.png");
-           System.out.println("Load anh thanh cong");
-           img =ImageIO.read(imgFile);
-//            int[] color= img.getRGB(0,0,img.getWidth(),img.getHeight(),null,0,160);
-//            int[][] grbMatrix = getRGB(img);
-//            int row = img.getHeight();
-//            int column = img.getWidth();
-//            showRGB(grbMatrix,0,1);
-//            showRGB(grbMatrix,1,1);
-//            showRGB(grbMatrix,2,1);
-//            showRGB(grbMatrix,row,column,1);
+        BufferedImage image = null;
+        image = ImageIO.read(new File("./res/huff_simple0.png"));
 
-        } catch (IOException e) {
-            System.out.println(e);
-        }
 
-        imageProcesser.LoadImage(img);
-        int [][] rgbArray = imageProcesser.getRGBArray(img);
-        int [][] redArray = imageProcesser.GetRedArray(rgbArray);
-        int [][] blueArray = imageProcesser.GetBlueArray(rgbArray);
-        int [][] greenArray = imageProcesser.GetGreenArray(rgbArray);
-        int [][] yArray = imageProcesser.ConvertRGBToY(redArray,blueArray,greenArray);
+        imageProcesser.LoadImage(image);
+        int rgb[][] = imageProcesser.getRGBArray();
+        int red[][] = imageProcesser.GetRedArray(rgb);
+        int green[][] = imageProcesser.GetGreenArray(rgb);
+        int blue[][] = imageProcesser.GetBlueArray(rgb);
+        int Y[][] = imageProcesser.ConvertRGBToY(red,green,blue);
+        int Cb[][] = imageProcesser.ConvertRGBToCb(red,green,blue);
+        int Cr[][] = imageProcesser.ConvertRGBToCr(red,green,blue);
+        Y = imageProcesser.shift(Y);
 
-        imageProcesser.PrintMatrix(rgbArray);
-        imageProcesser.PrintMatrix(redArray);
-        imageProcesser.PrintMatrix(blueArray);
-        imageProcesser.PrintMatrix(greenArray);
+        int quan[][] = imageProcesser.quant(Y);
+        PrintMatrix(quan);
 
-        BufferedImage temp = new BufferedImage(8,8, BufferedImage.TYPE_INT_RGB);
-        for (int i = 0; i < temp.getHeight(); i++) {
-            for (int j = 0; j < temp.getWidth(); j++) {
-
-                temp.setRGB(i,j, rgbArray[j][i]);
-            }
-        }
-        ImageIO.write(temp,"png", new File("res/ycolor.png"));
     }
 
-    public static int[][] getRGB(BufferedImage img) {
-        int[][] data = new int[img.getWidth()][img.getHeight()];
-        for (int i = 0; i < img.getHeight(); i++) {
-            for (int j = 0; j < img.getWidth(); j++) {
-                data[j][i] = img.getRGB(i,j);
-            }
-        }
-        return data;
-    }
 
-    public static void showRGB(int[][] grbMatrix, int x, int y) {
-        System.out.println();
-        int red = (grbMatrix[x][y] & 0xff0000) >>16;
-        System.out.println(red);
-
-        int green = (grbMatrix[x][y] & 0xff00) >> 8;
-        System.out.println(green);
-        int blue = (grbMatrix[x][y] & 0xff);
-        System.out.println(blue);
-    }
-    public static void showRGB(int[][] grbMatrix, int row, int column,int f) {
-        for (int i = 0; i < column; i++) {
-            for (int j = 0; j < row; j++) {
-                int red = (grbMatrix[i][j] & 0xff0000) >> 16;
-                System.out.print(red + " ");
-                int green = (grbMatrix[i][j] & 0xff00) >> 8;
-                System.out.print(green + " ");
-                int blue = (grbMatrix[i][j] & 0xff);
-                System.out.print(blue+ " \t");
+    public static void PrintMatrix(int[][] matrix) {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print(" | " + matrix[i][j]);
             }
             System.out.println();
         }
-
     }
+
 }
